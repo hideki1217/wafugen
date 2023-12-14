707badacbdd79613360cd583c68d457c5cd6ca6a
HidekiOkumura <okumurahidekikangeki@gmail.com>
2023-12-14 11:54:00 +0900

    Define report endpoint

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 # For Frontend Developers
 
 - How to run this server in your local machine
-```
-make run_backend
-```
+    ```
+    make run_backend
+    ```
 
 # For Backend Developers
 
@@ -13,4 +13,21 @@ make run_backend
   ./local_devenv.sh
   ```
 
-- 
+# API endpoint
+
+```
+GET/v1/report?youtube_ids = {str[]: Youtube公式識別子たち}
+
+return {
+    num_items: int,
+    reports: [
+        youtube_id: str, 
+        tsuri_score: int(lower=0, upper=100), 
+        tsuri_report: {*}
+    ]
+}
+
+// ExampleCommand: 
+// curl 'http://localhost:40000/v1/report?youtube_ids=\[youtube\]'
+```
+
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/v1/report")
+def create_report():
+    def parse_youtube_ids(s: str):
+        assert s[0] == "[" and s[-1] == "]"
+        return s[1:-1].split(",")
+
+    youtube_ids = parse_youtube_ids(request.args["youtube_ids"])
+
+    return [{
+        "youtube_id": youtube_id,
+        "tsuri_score": 50,
+        "tsuri_report": {
+            "example": 10
+        }
+    } for youtube_id in youtube_ids]
 
 
 app.run(host="0.0.0.0", port=40000, debug=True)
